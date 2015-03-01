b4552387516ebf661a35617d0b46279d9fae26da
Alexey Gaziev <alex.gaziev@gmail.com>
2015-03-01 15:01:14 +0800

    Start making web server

--- src/Server.hs
+++ src/Server.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Server where
 
 import Web.Scotty
-import Network.URI
+import Network.URI (parseURI)
+import Data.Monoid (mconcat)
 
-
+main = scotty 3000 $ do
+  get "/" $ do
+    html $ mconcat ["<h1>Hello, ", "world!</h1>"]
